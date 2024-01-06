59f148bd414ae2a04122f6d78f00cbf16375416b
Hasan Mir <seyed.hasan.mir@gmail.com>
2024-01-06 16:34:10 +0330

    replace vite with laravel-mix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,20 +3,14 @@ import "../css/app.css";
 
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.jsx`,
-            import.meta.glob("./pages/**/*.jsx")
-        ),
+    resolve: (name) => require(`./pages/${name}`),
     setup({ el, App, props }) {
         const root = createRoot(el);
-
         root.render(<App {...props} />);
     },
 });
